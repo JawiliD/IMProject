e410a72ff66aad051c8bbf402c3fbf57d656ecc3
dareajawili <dareajawil@gmail.com>
2022-12-17 21:00:37 +0800

    sql update

--- info.sql
+++ info.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 17, 2022 at 09:19 AM
+-- Generation Time: Dec 17, 2022 at 01:58 PM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
@@ -29,22 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `info` (
   `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `mobile` varchar(20) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `date` date NOT NULL,
+  `time` time(6) NOT NULL,
+  `content` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `info`
 --
 
-INSERT INTO `info` (`id`, `name`, `email`, `mobile`, `password`) VALUES
-(4, 'Darea Jawili', 'darea@jawili.com', '0928', '0928'),
-(5, 'John', 'Carlo@Blanco.com', '09242', 'hdegdd'),
-(6, 'Michaela', 'Michaela@Siena.com', '0935353', ' bgdvgd'),
-(7, 'Lee-Anne Monteray', 'Lee-Anne@Monterey.com', '09345678', 'asadarb'),
-(8, 'Bill Martillano', 'Bill@Martillano.com', '0975687', 'dlbhnqh');
+INSERT INTO `info` (`id`, `date`, `time`, `content`) VALUES
+(6, '2017-12-22', '12:53:44.000000', 'konti nalang matatapos rin to'),
+(7, '2017-12-22', '12:43:39.000000', 'Malapit na sa katotohanan'),
+(8, '2017-12-22', '12:44:52.000000', 'konti nalaaaaaaaaaannnnnngggggg'),
+(9, '2017-12-22', '12:47:32.000000', 'malapit naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+(10, '2017-12-22', '12:47:49.000000', 'tapos na rin sa wakas hahahahahahaha :)');
 
 --
 -- Indexes for dumped tables
@@ -64,7 +63,7 @@ ALTER TABLE `info`
 -- AUTO_INCREMENT for table `info`
 --
 ALTER TABLE `info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
